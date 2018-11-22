98ee9d05f5151f9915608a65d13c303c6dac0a32
Christian Rusin <christianrusinm@gmail.com>
2018-11-22 13:57:11 +0100

    Add project skeleton

---
+++ Trombi/Trombi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trombi
+//
+//  Created by Christian Rusin  on 22/11/2018.
+//  Copyright © 2018 Christian Rusin . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
