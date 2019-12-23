dbb1484b4f0375271f4ccd272b0782f3cd3fb7d7
Chris Rusin <christianrusinm@gmail.com>
2019-12-23 20:22:34 +0100

    Initialize UsefulLinks tab

---
+++ Trombi/Trombi/Application/Modules/UsefulLinks/UsefulLinksViewController.swift
@@ -0,0 +1,32 @@
+//
+//  UsefulLinksViewController.swift
+//  Trombi
+//
+//  Created by Chris Rusin on 12/23/19.
+//  Copyright © 2019 Christian Rusin . All rights reserved.
+//
+
+import UIKit
+
+class UsefulLinksViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Trombi/Trombi/Application/Modules/UsefulLinks/UsefulLinksViewViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  UsefulLinksViewViewModel.swift
+//  Trombi
+//
+//  Created by Chris Rusin on 12/23/19.
+//  Copyright © 2019 Christian Rusin . All rights reserved.
+//
+
+import Foundation
+
+class UsefulLinksViewViewModel {
+    
+}
